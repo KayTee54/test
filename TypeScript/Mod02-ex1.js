df56f98915635185fa9308971d1f364c85fd6450
KayTee54 <ginginyb@gmail.com>
2025-09-03 23:51:42 +0700

    for today

---
+++ TypeScript/Mod02-ex1.js
@@ -0,0 +1,17 @@
+var testId = "Dev";
+var maxwait = 30;
+var headless = true;
+var targerbrowser = ["chrome", "edge"];
+var httpstatus = [200, 204];
+var testresult = [true, false];
+var maxretry = 3;
+var captruescreenshot = true;
+//in ra console
+console.log("Test ID: ", testId);
+console.log("maxwait: ", maxwait);
+console.log("headless: ", headless);
+console.log("targerbrowser: ", targerbrowser);
+console.log("httpstatus: ", httpstatus);
+console.log("testresult: ", testresult);
+console.log("maxretry: ", maxretry);
+console.log("captruescreenshot: ", captruescreenshot);
---
+++ TypeScript/Mod02-ex1.ts
@@ -0,0 +1,17 @@
+let testId: string = "Dev";
+let maxwait: number = 30;
+let headless: boolean = true;
+let targerbrowser: string[] = ["chrome","edge"];
+let httpstatus: number[] = [200, 204];
+let testresult: boolean[] = [true, false];
+let maxretry: number= 3;
+let captruescreenshot: boolean = true;
+//in ra console
+console.log("Test ID: ", testId);
+console.log("maxwait: ", maxwait);
+console.log("headless: ", headless);
+console.log("targerbrowser: ", targerbrowser);
+console.log("httpstatus: ", httpstatus);
+console.log("testresult: ", testresult);
+console.log("maxretry: ", maxretry);
+console.log("captruescreenshot: ", captruescreenshot);
